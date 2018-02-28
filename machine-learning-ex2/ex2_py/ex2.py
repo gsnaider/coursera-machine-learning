d81b915530f9cd9c760f8d37931b6b6a531ce603
Gaston Snaider <gnsnaider@gmail.com>
2018-02-28 18:51:12 -0300

    Ex2 completed in python

--- machine-learning-ex2/ex2_py/ex2.py
+++ machine-learning-ex2/ex2_py/ex2.py
@@ -1,15 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from logreg import plot_data, cost
+from scipy.optimize import minimize
+from logreg import plot_data, cost, plot_decision_boundary, sigmoid, predict
 
 # ======================= Part 1: Plotting =======================
-print('Plotting data with + indicating (y = 1) examples and o indicating (y = 0) examples.');
+print('Plotting data with + indicating (y = 1) examples and o indicating (y = 0) examples.')
 
 data = np.loadtxt('../ex2/ex2data1.txt', delimiter=',')
 X = data[:,:2]
 y = data[:,2]
 plot_data(X, y)
-input('\nProgram paused. Press enter to continue.');
+input('\nProgram paused. Press enter to continue.')
 
 
 # =================== Part 2: Cost and Gradient descent ===================
@@ -26,25 +27,56 @@ X = ones
 initial_theta = np.zeros(n + 1)
 
 # Compute and display initial cost and gradient
-(J, grad) = cost(initial_theta, X, y);
+(J, grad) = cost(initial_theta, X, y)
 
-print('Cost at initial theta (zeros): %f' % J);
-print('Expected cost (approx): 0.693');
-print('Gradient at initial theta (zeros): ');
-print(grad);
-print('Expected gradients (approx):\n -0.1000\n -12.0092\n -11.2628');
+print('Cost at initial theta (zeros): %f' % J)
+print('Expected cost (approx): 0.693')
+print('Gradient at initial theta (zeros): ')
+print(grad)
+print('Expected gradients (approx):\n -0.1000\n -12.0092\n -11.2628')
 
 # Compute and display cost and gradient with non-zero theta
-test_theta = np.array([-24, 0.2, 0.2]);
-(J, grad) = cost(test_theta, X, y);
+test_theta = np.array([-24, 0.2, 0.2])
+(J, grad) = cost(test_theta, X, y)
 
-print('\nCost at test theta: %f' % J);
-print('Expected cost (approx): 0.218');
-print('Gradient at test theta: ');
-print(grad);
-print('Expected gradients (approx):\n 0.043\n 2.566\n 2.647');
+print('\nCost at test theta: %f' % J)
+print('Expected cost (approx): 0.218')
+print('Gradient at test theta: ')
+print(grad)
+print('Expected gradients (approx):\n 0.043\n 2.566\n 2.647')
 
-input('\nProgram paused. Press enter to continue.');
+input('\nProgram paused. Press enter to continue.')
 
 
 
+## ============= Part 3: Optimizing using minimize =============
+
+
+res = minimize(cost, initial_theta, (X,y), method='BFGS',jac=True) 
+theta = res.x
+J = res.fun
+
+# Print theta to screen
+print('Cost at theta found by fminunc: %f' % J)
+print('Expected cost (approx): 0.203')
+print('theta: ')
+print(theta)
+print('Expected theta (approx):')
+print(' -25.161\n 0.206\n 0.201')
+
+# Plot Boundary
+plot_decision_boundary(theta, X, y)
+
+input('\nProgram paused. Press enter to continue.')
+
+# ============== Part 4: Predict and Accuracies ==============
+
+prob = sigmoid(np.array([1, 45, 85]).dot(theta));
+print('For a student with scores 45 and 85, we predict an admission probability of %f' % prob);
+print('Expected value: 0.775 +/- 0.002\n');
+
+# Compute accuracy on our training set
+p = predict(theta, X);
+
+print('Train Accuracy: %f' % (np.mean(p == y) * 100));
+print('Expected accuracy (approx): 89.0');
--- machine-learning-ex2/ex2_py/logreg.py
+++ machine-learning-ex2/ex2_py/logreg.py
@@ -1,14 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def plot_data(X, y):
+def plot_data(X, y, legends=[]):
 	pos_x = X[y==1]
 	neg_x = X[y==0]
 	plt.plot(pos_x[:,0], pos_x[:,1], 'k+')
 	plt.plot(neg_x[:,0], neg_x[:,1], 'yo')
 	plt.xlabel('Exam 1 score')
 	plt.ylabel('Exam 2 score')
-	plt.legend(['Admitted','Not admitted'])
+	legends.extend(['Admitted','Not admitted'])
+	plt.legend(legends)
 	plt.show()
 
 def sigmoid(z):
@@ -19,3 +20,14 @@ def cost(theta, X, y):
 	J = -1 / m * (y.T.dot(np.log(sigmoid(X.dot(theta)))) + (1 -y).T.dot(np.log(1 - sigmoid(X.dot(theta)))))
 	grad = 1 / m * X.T.dot(sigmoid(X.dot(theta)) - y)
 	return (J, grad)
+
+def plot_decision_boundary(theta, X, y):
+	#Two points to define the line
+	plot_x1 = np.array([np.min(X[:,1]) - 2, np.max(X[:,1]) + 2])
+	plot_x2 = -1 / theta[2] * (theta[0] + plot_x1 * theta[1])
+	plt.plot(plot_x1, plot_x2)
+	plot_data(X[:,1:], y, ['Desicion boundary'])
+
+def predict(theta, X):
+	return sigmoid(X.dot(theta)) >= 0.5
+
